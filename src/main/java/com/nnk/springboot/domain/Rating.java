dcfbd52a744d8b51cf36d64e64918400570d935b
Dominique Pokrzywa <S223347@siege.axa-fr.intraxa>
2020-10-11 11:33:06 +0200

    Initial commit

---
+++ src/main/java/com/nnk/springboot/domain/Rating.java
@@ -0,0 +1,60 @@
+package com.nnk.springboot.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "rating")
+public class Rating {
+
+    Integer id ;
+    String moodysRating ;
+    String sandPRating ;
+    String fitchRating ;
+    Integer orderNumber ;
+
+    public Rating(String moodysRating, String sandPRating, String fitchRating, int i) {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMoodysRating() {
+        return moodysRating;
+    }
+
+    public void setMoodysRating(String moodysRating) {
+        this.moodysRating = moodysRating;
+    }
+
+    public String getSandPRating() {
+        return sandPRating;
+    }
+
+    public void setSandPRating(String sandPRating) {
+        this.sandPRating = sandPRating;
+    }
+
+    public String getFitchRating() {
+        return fitchRating;
+    }
+
+    public void setFitchRating(String fitchRating) {
+        this.fitchRating = fitchRating;
+    }
+
+    public Integer getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(Integer orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+}
